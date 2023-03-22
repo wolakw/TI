7055597b3fe3d5aa29e3c5f9c9e05a30e694b4ca
wolakw <wojtek.2017w@gmail.com>
2023-03-22 22:54:41 +0100

    kwadratowe done

--- .idea/kwadratowe.js
+++ .idea/kwadratowe.js
@@ -1,4 +1,4 @@
-kwadratowe = function (a,b,c) {
+kwadratowe = function (a, b, c) {
     this.A = a;
     this.B = b;
     this.C = c;
@@ -17,7 +17,7 @@ kwadratowe.prototype.miejscaZerowe = function () {
 
     if (d > 0) {
         this.x1 = ((this.B * -1) - Math.sqrt(d)) / (2 * this.A);
-        this.x1 = ((this.B * -1) + Math.sqrt(d)) / (2 * this.A);
+        this.x2 = ((this.B * -1) + Math.sqrt(d)) / (2 * this.A);
     } else if (d == 0) {
         this.x1 = (this.B * -1) / (2 * this.A);
         this.x2 = null;
@@ -58,3 +58,56 @@ kwadratowe.prototype.licz = function () {
     }
 }
 
+function podpiecie() {
+    let element = document.getElementById('licz');
+    element.addEventListener('click', function (event) {
+        let a = document.getElementById('wsp_a').value;
+        let b = document.getElementById('wsp_b').value;
+        let c = document.getElementById('wsp_c').value;
+
+        let rownanie = new kwadratowe(a,b,c);
+        rownanie.licz();
+
+        if (rownanie.getDelta() == null)
+            return false;
+
+        let lista = document.getElementById('lista');
+        let x1 = null;
+        let x2 = null;
+
+        if (lista.hasChildNodes()) {
+            while (lista.firstChild) {
+                lista.removeChild(lista.firstChild);
+            }
+        }
+
+        let wynik = document.createTextNode('Wynik: ');
+
+        if (rownanie.getDelta() > 0) {
+            x1 = 'x\u2081 = ' + rownanie.getX1();
+            x2 = 'x\u2082 = ' + rownanie.getX2();
+        } else if (rownanie.getDelta() == 0) {
+            x1 = 'x = ' + rownanie.getX1();
+        } else {
+            x1 = 'Brak miejsc zerowych, \u0394 < 0';
+        }
+
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+
+        let liText = document.createTextNode(x1);
+        li.appendChild(liText);
+        ul.appendChild(li);
+
+        lista.appendChild(wynik);
+        lista.appendChild(ul);
+
+        if(x2) {
+            let li2 = document.createElement('li');
+            let liText2 = document.createTextNode(x2);
+
+            li2.appendChild(liText2);
+            ul.appendChild(li2);
+        }
+    });
+}
